7d64a97a44c024c37fc4a71b08e92814a8584c4e
Gladys <gladysalu1@gmail.com>
2024-02-11 00:03:19 +0100

    commit

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,17 +1,27 @@
+import { useState } from "react"
 import TodoList from "./TodoList"
+import { useDispatch } from "react-redux";
 
 const Todo = () => {
+  const [input, setInput] = useState("");
+  const dispatch = useDispatch()
+
+  const handleInput = (e) => {
+    console.log(e.target.value);
+    setInput(e.target.value)
+  };
   return (
-    <div className="flex flex-col justify-center items-center">
-      <div className="flex gap-1 ">
+    <div className="flex flex-col justify-center m-10 w-screen overflow-x-hidden items-center">
+      <div className="flex gap-1 w-full justify-center items-center">
         <input
+          onChange={handleInput}
           type="text"
           placeholder="Type here"
           className="input input-bordered  input-md input-accent w-full max-w-xs"
         />
-        <button
+        <button onClick={() =>dispatch (addTodo(input))}
   className="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
-  Button
+  Add
 </button>
 </div>
 <TodoList />
--- src/store/todoSlice.js
+++ src/store/todoSlice.js
@@ -1,17 +1,16 @@
-import {createSlice} from "@reduxjs/toolkit"     
+import {createSlice} from "@reduxjs/toolkit" 
 
 
 export const todoSlice= createSlice(
-
     {
         name:"todo",
-        initialState: {todoList: [{id:1, text: "hello world"}]}
+        initialState: {todoList: [{id:1, text: "hello world", completed:false }]}
         reducers:{
                
             decrement: (state)=>{
                 state.counter === 0 ? state.counter = 0 : state.counter-=1
                 state.name="GMC 3"
             }
-    }
+    } 
 }
 )
